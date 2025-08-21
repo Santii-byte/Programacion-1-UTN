e880e359627c0e1adfaaf87f44caedc238b84aeb
Santii-byte <baronenunezsantino2@gmail.com>
2025-08-21 08:15:49 -0300

    .

--- EJERCICIO 1.py
+++ EJERCICIO 1.py
@@ -1,33 +1,6 @@
-numero1=20
-numero2=10.5
-suma=numero1+numero2
-resta=numero1-numero2
-multi=numero1*numero2
-div=numero1/numero2
-print(suma,resta,multi,div)
-nombre="maximo"
-precio=99.99
-descuento=0.25
-precio_total=precio*descuento
-
-cadena="hola"
-longitud=len(cadena)
-
-precio3=99.99
-precio4=int(precio)
-
-nombre1="Maximo"
-apellido="Quiroga"
-concatenar=nombre1+" "+apellido
-
-edad=18
-edad=edad-5
-edad=edad+10
-
-altura=1.73
-altura1=altura*4/3
-
-nombre5="maximo"
-nombre6=nombre5.upper()
-nombre5=nombre5.lower()
-nombre7=nombre6[0]+nombre6[1,5].lower()
+monto=100
+propina1=monto+monto*0.1
+propina2=monto+monto*0.15
+print(f"Su monto es de ${monto}")
+print(f"Con 10% de propina su monto total queda en ${propina1}")
+print(f"Con 15% de propina su monto total queda en ${propina2}")
