236a435d81a962801b500ad015bfa6b3f337856c
Santii-byte <baronenunezsantino2@gmail.com>
2025-08-21 09:20:54 -0300

    are

--- prestamo.py
+++ prestamo.py
@@ -1,1 +1,5 @@
-monto=input("Ingrese el monto que desa pedir: ")
+monto=float(input("Ingrese el monto que desea pedir: "))
+interes_mensual=monto+monto*0.02
+interes_anual=monto+monto*0.24
+print(f"Su monto total a devolver es de ${interes_anual}")
+print(f"Su monto mensual con interes es de ${interes_mensual}")dfhdhsgdfggf
