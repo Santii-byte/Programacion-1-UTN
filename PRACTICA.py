1fa8623c943751e65d5317b191a0a5c1099bb234
Santii-byte <baronenunezsantino2@gmail.com>
2025-08-21 00:13:12 -0300

    Update PRACTICA.py

--- PRACTICA.py
+++ PRACTICA.py
@@ -4,22 +4,23 @@ suma=numero1+numero2
 resta=numero1-numero2
 multi=numero1*numero2
 div=numero1/numero2
-print(suma,resta,multi,div)
+print(f"Suma: {suma}, resta: {resta}, multi: {multi}, div: {div}")
 nombre="maximo"
 precio=99.99
 descuento=0.25
 precio_total=precio*descuento
+print(f"Precio con descuento: {precio_total}")
 
 cadena="hola"
 longitud=len(cadena)
-
+print(f"Longitud de cadena: {longitud}")
 precio3=99.99
 precio4=int(precio)
-
+print(f"Precio pasado a entero {precio}")
 nombre1="Maximo"
 apellido="Quiroga"
 concatenar=nombre1+" "+apellido
-
+print(concatenar)
 edad=18
 edad=edad-5
 edad=edad+10
@@ -27,7 +28,8 @@ edad=edad+10
 altura=1.73
 altura1=altura*4/3
 
-nombre5="maximo"
-nombre6=nombre5.upper()
-nombre5=nombre5.lower()
-nombre7=nombre6[0]+nombre6[1,5].lower()
+nombre11="maximo"
+nombre11=nombre11.upper()
+nombre12=nombre11.lower()
+nombre13=nombre11[0]+nombre12[1:6].lower()
+print(nombre13)
